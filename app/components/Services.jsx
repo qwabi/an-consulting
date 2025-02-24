dad17114dcdccd5809fdb4dafc16554c37c2f7cc
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-02-24 11:18:47 +0200

    adjust to use cards

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -7,24 +7,77 @@ import Link from 'next/link';
 const services = [
   {
     title: 'Audit & Assurance',
-    description:
-      "Ensure compliance and financial accuracy with our thorough auditing services. We provide comprehensive auditing solutions that help maintain your business's financial integrity.",
+    description: `Ensure compliance and financial accuracy with our thorough auditing services. We provide comprehensive auditing solutions that help maintain your business's financial integrity. 
+      <ul class='list-disc pl-5  p-4 rounded-md'>
+                  <li>External audits</li>
+                  <li>Quality assurance reviews</li>
+                  <li>Agreed upon procedures</li>
+                  <li>Internal audits</li>
+                </ul>`,
     icon: FileText,
     image:
       'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/audit.jpg-PFML6JuyjqcuoLJ8mNj6xT3BNoBvBz.jpeg',
   },
   {
-    title: 'Tax Planning',
-    description:
-      'Strategic tax planning and preparation services to optimize your financial position. We help you navigate complex tax regulations while maximizing your benefits.',
+    title: 'Tax',
+    description: `<ul class='list-disc pl-5  p-4 rounded-md'>
+                  <li>
+                    Preparation and review of tax computations and returns
+                  </li>
+                  <li>VAT and other indirect taxes</li>
+                  <li>Tax planning</li>
+                </ul>`,
     icon: PieChart,
     image:
       'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/planning.jpg-HazfVNpqUoupZNlroKwzyKwkOcXtZM.jpeg',
   },
   {
+    title: 'Business Consulting',
+    description: `             
+                <ul class='list-disc pl-5  p-4 rounded-md'>
+                  <li>
+                    Preparation and compilation of IFRS and GRAP compliant
+                    financial statements
+                  </li>
+                  <li>
+                    Review of financial statements for compliance with IFRS and
+                    GRAP standards
+                  </li>
+                  <li>
+                    Preparation and maintenance of IFRS and GRAP compliant asset
+                    register
+                  </li>
+                  <li>IFRS and GRAP technical training</li>
+                </ul>
+              </li>`,
+    icon: FileText,
+    image:
+      'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/audit.jpg-PFML6JuyjqcuoLJ8mNj6xT3BNoBvBz.jpeg',
+  },
+  {
     title: 'Financial Advisory',
-    description:
-      'Expert financial guidance to help your business grow and thrive. Our advisory services provide insights and strategies for sustainable business growth.',
+    description: `Expert financial guidance to help your business grow and thrive. Our advisory services provide insights and strategies for sustainable business growth.
+      <ul class='list-disc pl-5  p-4 rounded-md'>
+                  <li>
+                    <span className='text-lg font-bold'>
+                      Outsourced accounting services
+                    </span>
+                    <ul class='list-disc pl-5  p-4 rounded-md''>
+                      <li>
+                        Monthly outsourced accounting packages
+                        <ul class='list-disc pl-5  p-4 rounded-md'>
+                          <li>
+                            We provide accounting support for small businesses.
+                            Are packages are tailored to the needs of start-ups,
+                            fairly new and established businesses. We take care
+                            of the accounting, giving you time to focus on
+                            growing the business.
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>`,
     icon: TrendingUp,
     image:
       'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/advice.jpg-CYUqexmu46jOyh3HrEcbr3ZNva7Aaz.jpeg',
@@ -46,50 +99,7 @@ export default function Services() {
         <h2 className='text-3xl md:text-4xl font-bold text-center mb-12 text-[#233142]'>
           About us
         </h2>
-        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {services.map((service, index) => (
-            <motion.div
-              key={service.title}
-              className="bg-white rounded-lg shadow-lg overflow-hidden"
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: index * 0.1 }}
-              viewport={{ once: true }}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-            >
-              <div className="relative h-48">
-                <img
-                  src={service.image || "/placeholder.svg"}
-                  alt={service.title}
-                  className="w-full h-full object-cover"
-                />
-                <div className="absolute inset-0 bg-gradient-to-t from-[#233142] via-transparent to-transparent flex items-end justify-start p-4">
-                  <service.icon className="w-8 h-8 text-[#fac420]" />
-                </div>
-              </div>
-              <div className="p-6">
-                <h3 className="text-xl font-semibold mb-2 text-[#233142]">
-                  {service.title}
-                </h3>
-                <p className="text-[#7591a0] mb-4">{service.description}</p>
-                <motion.button
-                  whileHover={{ scale: 1.05 }}
-                  whileTap={{ scale: 0.95 }}
-                >
-                  <Link
-                    className="bg-[#fac420] text-[#233142] px-4 py-2 rounded-full font-bold text-sm hover:bg-opacity-90 transition-all duration-300"
-                    href="/#packages"
-                  >
-                    {" "}
-                    Learn More
-                  </Link>
-                </motion.button>
-              </div>
-            </motion.div>
-          ))}
-        </div> */}
-        <div className='w-full p-6 md:p-4 md:max-w-2xl mx-auto text-lg'>
+        <div className='w-full p-6 md:p-4 my-6 mx-auto text-xl'>
           <p>
             Our team is made up of qualified and experience professionals
             comprising chartered accountants, registered auditors and other
@@ -99,84 +109,52 @@ export default function Services() {
             play a role to assist them reach their potential and grow. Get in
             touch with us, we might just be what you are looking for.
           </p>
-          <h2 className='text-4xl font-bold text-center mt-8 text-[#233142]'>
-            Our services
-          </h2>
-          <p>
-            <ul className='ml-2 p-2'>
-              <li>
-                <span className='text-xl font-bold'>Audit and assurance</span>
-                <ul className='ml-4 p-2'>
-                  <li>External audits</li>
-                  <li>Quality assurance reviews</li>
-                  <li>Agreed upon procedures</li>
-                </ul>
-              </li>
-              <li>
-                <span className='text-xl font-bold'>Tax</span>
-                <ul className='ml-4 p-2'>
-                  <li>
-                    Preparation and review of tax computations and returns
-                  </li>
-                  <li>VAT and other indirect taxes</li>
-                  <li>Tax planning</li>
-                </ul>
-              </li>
-              <li>
-                <span className='text-xl font-bold'>Advisory</span>
-                <ul className='ml-4 p-2'>
-                  <li>
-                    <span className='text-lg font-bold'>
-                      Outsourced accounting services
-                    </span>
-                    <ul className='ml-6 p-2'>
-                      <li>
-                        Monthly outsourced accounting packages
-                        <ul className='ml-6 p-2'>
-                          <li>
-                            We provide accounting support for small businesses.
-                            Are packages are tailored to the needs of start-ups,
-                            fairly new and established businesses. We take care
-                            of the accounting, giving you time to focus on
-                            growing the business.
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <span className='text-lg font-bold'>Payroll</span>
-                <ul className='ml-4 p-2'>
-                  <li>Outsourced payroll administration</li>
-                </ul>
-              </li>
-              <li>
-                <span className='text-xl font-bold'>Business consulting</span>
-                <ul className='ml-4 p-2'>
-                  <li>
-                    Preparation and compilation of IFRS and GRAP compliant
-                    financial statements
-                  </li>
-                  <li>
-                    Review of financial statements for compliance with IFRS and
-                    GRAP standards
-                  </li>
-                  <li>
-                    Preparation and maintenance of IFRS and GRAP compliant asset
-                    register
-                  </li>
-                  <li>IFRS and GRAP technical training</li>
-                </ul>
-              </li>
-              <li>
-                <span className='text-xl font-bold'>
-                  Internal audit service
-                </span>
-              </li>
-            </ul>
-          </p>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+          {services.map((service, index) => (
+            <motion.div
+              key={service.title}
+              className='bg-white rounded-lg shadow-lg overflow-hidden h-full'
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+              viewport={{ once: true }}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <div className='relative h-48'>
+                <img
+                  src={service.image || '/placeholder.svg'}
+                  alt={service.title}
+                  className='w-full h-full object-cover'
+                />
+                <div className='absolute inset-0 bg-gradient-to-t from-[#233142] via-transparent to-transparent flex items-end justify-start p-4'>
+                  <service.icon className='w-8 h-8 text-[#fac420]' />
+                </div>
+              </div>
+              <div className='p-6 inline-flex flex-col justify-between min-h-96 items-between'>
+                <h3 className='text-xl font-semibold mb-2 text-[#233142]'>
+                  {service.title}
+                </h3>
+                <p
+                  className='text-[#7591a0] mb-4'
+                  dangerouslySetInnerHTML={{ __html: service.description }}
+                />
+                <motion.button
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                >
+                  <Link
+                    className='bg-[#fac420] text-[#233142] px-4 py-2 rounded-full font-bold text-sm hover:bg-opacity-90 transition-all duration-300'
+                    href='/#packages'
+                  >
+                    {' '}
+                    Learn More
+                  </Link>
+                </motion.button>
+              </div>
+            </motion.div>
+          ))}
         </div>
       </motion.div>
     </section>
